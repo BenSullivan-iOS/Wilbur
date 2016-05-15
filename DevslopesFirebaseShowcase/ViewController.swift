33d33c9cb1a16431be6875a26fae96dee332353a
Ben Sullivan <benskill@gmail.com>
2016-05-15 20:04:41 +0100

    Initial Commit

---
+++ DevslopesFirebaseShowcase/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DevslopesFirebaseShowcase
+//
+//  Created by Ben Sullivan on 15/05/2016.
+//  Copyright © 2016 Sullivan Applications. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
