f0ca6590c4dd82fe8cbb058932fd00face500fe4
Ben Sullivan <benskill@gmail.com>
2016-06-21 13:15:38 +0100

    Create post shows image in correct aspect

--- DevslopesFirebaseShowcase/CreatePostTest.swift
+++ DevslopesFirebaseShowcase/CreatePostTest.swift
@@ -7,19 +7,93 @@
 //
 
 import UIKit
+import AVFoundation
 
-class CreatePostTest: UIViewController, UITextViewDelegate {
+class ImageTable: UITableViewCell {
+  
+  @IBOutlet weak var tableImage: UIImageView!
+  
+}
+
+class CreatePostTest: UIViewController, UITextViewDelegate, UIGestureRecognizerDelegate, UIScrollViewDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate, UITableViewDelegate {
   
   @IBOutlet weak var descriptionText: UITextView!
   @IBOutlet weak var image: UIImageView!
+  @IBOutlet weak var cameraIcon: UIButton!
+  @IBOutlet weak var micIcon: UIButton!
+  @IBOutlet weak var scrollView: UIScrollView!
+  @IBOutlet weak var selectedImage: UIImageView!
+  @IBOutlet weak var tableView: UITableView!
+  
+  private let imagePicker = UIImagePickerController()
+  private var selectedImagePath = NSURL?()
+  
+  func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCellWithIdentifier("imageCell") as! ImageTable
+  
+    if selectedImage.image != nil {
+    cell.tableImage.image = selectedImage.image
+    }
+    
+    return cell
+    
+  }
+ 
+  func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return 1
+  }
+  
+  @IBAction func takePhotoButtonPressed(sender: AnyObject) {
+    
+    if UIImagePickerController.isSourceTypeAvailable(.Camera) {
+      imagePickerAlert()
+    } else {
+      presentViewController(imagePicker, animated: true, completion: nil)
+    }
+  }
   
   func textViewDidBeginEditing(textView: UITextView) {
     descriptionText.text = ""
     descriptionText.textColor = .grayColor()
   }
   
+  func scrollViewDidScroll(scrollView: UIScrollView) {
+    
+    
+  }
+  
+  func tapReceived() {
+    
+    print("tapped")
+    
+    self.view.endEditing(true)
+  }
+  
+  func tapGestureRecogniser() {
+    
+    let tap = UITapGestureRecognizer(target: self, action: #selector(self.tapReceived))
+    tap.numberOfTapsRequired = 1
+    tap.enabled = true
+
+    self.view.addGestureRecognizer(tap)
+    
+  }
+  
   override func viewDidLoad() {
     super.viewDidLoad()
+    
+    tableView.delegate = self
+//    tableView.rowHeight = UITableViewAutomaticDimension
+    
+    scrollView.scrollEnabled = false
+    
+    tapGestureRecogniser()
+    
+    scrollView.delegate = self
+    cameraIcon.imageView?.contentMode = .ScaleAspectFit
+    micIcon.imageView?.contentMode = .ScaleAspectFit
+
+    imagePicker.delegate = self
     
     descriptionText.delegate = self
     descriptionText.layer.cornerRadius = 3.0
@@ -27,7 +101,75 @@ class CreatePostTest: UIViewController, UITextViewDelegate {
     descriptionText.layer.borderColor = UIColor(colorLiteralRed: 170/255, green: 170/255, blue: 170/255, alpha: 0.2).CGColor
   }
   
-  override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
-    self.view.endEditing(true)
+  //MARK: - CAMERA FUNCTIONS
+  
+  func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+
+    scrollView.scrollEnabled = true
+    
+    dismissViewControllerAnimated(true, completion: nil)
+    
+    let image = info[UIImagePickerControllerOriginalImage] as? UIImage
+    selectedImage.image = image
+    
+    let saveDirectory = String(HelperFunctions.getDocumentsDirectory()) + "/images/tempImage.jpg"
+    
+    let tempImage = info[UIImagePickerControllerOriginalImage] as! UIImage
+    
+    saveImage(tempImage, path: saveDirectory)
+    
+    print("Did finish picking image - ", saveDirectory)
+    let height = AVMakeRectWithAspectRatioInsideRect((image?.size)!, selectedImage.frame).height
+
+    tableView.rowHeight = height
+    tableView.reloadData()
+  }
+  
+  func resizeImage(image: UIImage, newWidth: CGFloat) -> UIImage {
+    
+    let scale = newWidth / image.size.width
+    let newHeight = image.size.height * scale
+    UIGraphicsBeginImageContext(CGSizeMake(newWidth, newHeight))
+    image.drawInRect(CGRectMake(0, 0, newWidth, newHeight))
+    let newImage = UIGraphicsGetImageFromCurrentImageContext()
+    UIGraphicsEndImageContext()
+    
+    return newImage!
+  }
+  
+  func saveImage (image: UIImage, path: String) -> Bool {
+    
+    let compressedImage = resizeImage(image, newWidth: 1536)
+    let jpgImageData = UIImageJPEGRepresentation(compressedImage, 0)
+    let result = jpgImageData!.writeToFile(String(path), atomically: true)
+    
+    selectedImagePath = NSURL(fileURLWithPath: path)
+    
+    return result
+  }
+  
+  func imagePickerAlert() {
+    
+    let alert = UIAlertController(title: "Choose source type", message: "", preferredStyle: .ActionSheet)
+    alert.popoverPresentationController?.sourceView = self.view
+    
+    alert.addAction(UIAlertAction(title: "Take photo", style: .Default, handler: { action in
+      
+      self.imagePicker.sourceType = .Camera
+      self.presentViewController(self.imagePicker, animated: true, completion: nil)
+      
+    }))
+    
+    
+    alert.addAction(UIAlertAction(title: "Photo library", style: .Default, handler: { action in
+      
+      self.imagePicker.sourceType = .PhotoLibrary
+      self.presentViewController(self.imagePicker, animated: true, completion: nil)
+      
+    }))
+    
+    alert.addAction(UIAlertAction(title: "Cancel", style: .Cancel, handler: nil))
+    
+    presentViewController(alert, animated: true, completion: nil)
   }
 }
