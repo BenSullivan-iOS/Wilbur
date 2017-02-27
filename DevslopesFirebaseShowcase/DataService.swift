ed9c44e24070d70f0e9817c6c451e5b519a62977
Ben Sullivan <ben.sullivan@service-as-imac.local>
2017-02-27 15:46:40 +0000

    Removed old code

--- DevslopesFirebaseShowcase/DataService.swift
+++ DevslopesFirebaseShowcase/DataService.swift
@@ -29,439 +29,6 @@ struct DataStruct {
   }
 }
 
-//struct DataService: HelperFunctions {
-//  
-//  weak var delegate: PostCellDelegate? = nil
-//    
-//  private var _REF_BASE = URL_BASE
-//  private var _REF_POSTS = URL_BASE.child("posts")
-//  private var _REF_USERS = URL_BASE.child("users")
-//  private var _posts = [Post]()
-//  private var _answeredPosts: [Post]!
-//  private var _myPosts: [Post]!
-//  private var _currentUserKey: String?
-//  private var _usernames: [String: String]!
-//  private var count = 0
-//  
-//  var currentUserKey: String? {
-//    
-//    return NSUserDefaults.standardUserDefaults().valueForKey(Constants().KEY_UID) as? String ?? nil
-//  }
-//  
-//  var usernames: [String: String] { return _usernames }
-//  
-//  var posts: [Post] {
-//    
-//    get {
-//    return _posts
-//    }
-//    set {
-//      _posts = newValue
-//    }
-////
-////    if let postArray = _posts {
-////      
-////      
-////      return postArray
-////    }
-////    
-////    
-////    return [Post]()
-//  }
-//  
-//  var answeredPosts: [Post] {
-//    
-//    if let postArray = _answeredPosts {
-//      return postArray
-//    }
-//    return [Post]()
-//  }
-//  
-//  var myPosts: [Post] {
-//    
-//    let allPosts = _posts + _answeredPosts
-//    
-//    let posts = allPosts.filter { $0.userKey == currentUserKey }
-//    
-//    return posts
-//  }
-//  
-//  var REF_BASE : FIRDatabaseReference { return _REF_BASE }
-//  var REF_POSTS: FIRDatabaseReference { return _REF_POSTS }
-//  var REF_USERS: FIRDatabaseReference { return _REF_USERS }
-//  
-//  var REF_USER_CURRENT: FIRDatabaseReference {
-//    
-//    if let userKey = currentUserKey {
-//      
-//      return URL_BASE.child("users").child(userKey)
-//    } else {
-//      
-//      return URL_BASE.child("users").child("guest")
-//      
-//    }
-//  }
-//  
-//  mutating func deletePostAtIndex(index: Int) {
-//    
-//    _posts.removeAtIndex(index)
-//  }
-//  
-//  mutating func deleteAnsweredPostAtIndex(index: Int) {
-//    
-//    _answeredPosts.removeAtIndex(index)
-//  }
-//  
-//  mutating func deletePostsByBlockedUser(userKey: String) {
-//    
-//    _posts = _posts.filter { $0.userKey != userKey }
-//  }
-//  
-//  func createFirebaseUser(uid: String, user: [String:String]) {
-//    
-//    REF_USERS.child(uid).setValue(user)
-//    
-//  }
-//  
-//  func isAfterDate(startDate: NSDate, endDate: NSDate) -> Bool {
-//    
-//    let calendar = NSCalendar.currentCalendar()
-//    
-//    let components = calendar.components([.Second],
-//                                         fromDate: startDate,
-//                                         toDate: endDate.dateByAddingTimeInterval(86400),
-//                                         options: [])
-//    
-//    if components.day > 0 {
-//      return true
-//    } else {
-//      return false
-//    }
-//  }
-//  
-//  
-//  //MARK: - DOWNLOAD CONTENT
-//  
-//  mutating func downloadTableContent() {
-//    
-//    downloadUsernamesForComments()
-//    
-//    var blockedUsers = [String]()
-//    
-//    guard currentUserKey != nil else {
-//      downloadPosts([""]); return }
-//    
-//    let userRef = REF_USER_CURRENT
-//    
-//    userRef.observeEventType(.Value, withBlock: { snapshot in
-//      
-//      if let snapshots = snapshot.children.allObjects as? [FIRDataSnapshot] {
-//        
-//        for snap in snapshots {
-//          
-//          if let userDict = snap.value as? [String: AnyObject] {
-//            
-//            if snap.key == "blockedUsers" {
-//              
-//              for i in userDict {
-//                
-//                blockedUsers.append(i.0)
-//              }
-//            }
-//            
-//            if snap.key == "profileImage" {
-//              
-//              for i in userDict {
-//                
-//                self.downloadProfileImage(i.0)
-//              }
-//            }
-//            
-//          }
-//        }
-//        self.downloadPosts(blockedUsers)
-//      }
-//    })
-//    
-//  }
-//  
-//  mutating func downloadUsernamesForComments() {
-//    
-//    let userRef = REF_USERS
-//    
-//    userRef.observeEventType(.Value, withBlock: { snapshot in
-//      
-//      self._usernames = [:]
-//      
-//      if let snapshots = snapshot.children.allObjects as? [FIRDataSnapshot] {
-//        
-//        for snap in snapshots {
-//          
-//          if let userDict = snap.value as? [String: AnyObject] {
-//            
-//            if let name = userDict["username"] as? String {
-//              print(name, snap.key)
-//              self._usernames[snap.key] = name
-//            }
-//          }
-//        }
-//      }
-//    })
-//  }
-//  
-//  mutating func downloadPosts(blockedUsers: [String]) {
-//    
-//    REF_POSTS.observeEventType(.Value, withBlock: { snapshot in
-//      
-//      self._posts = []
-//      self._answeredPosts = []
-//      
-//      if let snapshots = snapshot.children.allObjects as? [FIRDataSnapshot] {
-//        
-//        for snap in snapshots {
-//          
-//          if let postDict = snap.value as? [String: AnyObject] {
-//            print(postDict)
-//            let key = snap.key
-//            let post = Post(postKey: key, dictionary: postDict)
-//            
-//            if post.answered == "" {
-//              
-//              if !blockedUsers.contains(post.userKey) {
-//                
-//                self._posts.append(post)
-//              }
-//            } else {
-//              
-//              self._answeredPosts.append(post)
-//              //create array for answered table or just filter other array?
-//            }
-//          }
-//        }
-//        
-//        self._posts.sortInPlace({ (first, second) -> Bool in
-//          
-//          let df = NSDateFormatter()
-//          df.dateFormat = "yyyy-MM-dd HH:mm:ss ZZZZ"
-//          
-//          if let firstDate = df.dateFromString(first.date), secondDate = df.dateFromString(second.date) {
-//            
-//            return self.isAfterDate(firstDate, endDate: secondDate)
-//          }
-//          
-//          return true
-//        })
-//        
-//        //only download if not in cache already?
-//        
-//        if !self._posts.isEmpty {
-//          
-//          self.downloadImage(self.posts)
-//        }
-//        //Received by CommentsVC
-//        NSNotificationCenter.defaultCenter().postNotificationName("updateComments", object: nil)
-//        NSNotificationCenter.defaultCenter().postNotificationName("reloadTables", object: nil)
-//
-//        self.delegate?.reloadTable2(self._posts)
-//      }
-//    })
-//  }
-//  
-//  mutating func downloadImage(posts: [Post]) {
-//    print("POSTS COUNT", count)
-//    print(posts[count])
-//    
-//    guard let imageLocation = posts[count].imageUrl else {
-//      print("no image")
-//      
-//      if self.count < posts.count - 1 {
-//        if self.count == 1 {
-//          self.delegate?.reloadTable()
-//        }
-//        self.count += 1
-//        self.downloadImage(self.posts)
-//      } else {
-//        self.count = 0
-//      }
-//      
-//      return }
-//    
-//    guard Cache.shared.imageCache.objectForKey(imageLocation) as? UIImage == nil else {
-//      print("image already downloaded")
-//      
-//      if self.count < posts.count - 1 {
-//        
-//        self.count += 1
-//        self.downloadImage(self.posts)
-//      } else {
-//        self.count = 0
-//      }
-//      return }
-//    
-//    let saveLocation = NSURL(fileURLWithPath: docsDirect() +  imageLocation)
-//    let storageRef: FIRStorageReference? = FIRStorage.storage().reference()
-//    
-//    guard let storage = storageRef else { return }
-//    
-//    let pathReference = storage.child(imageLocation)
-//    
-//    pathReference.writeToFile(saveLocation) { (URL, error) -> Void in
-//      
-//      guard let URL = URL where error == nil else { print("Data Service download error", error.debugDescription); return }
-//      
-//      if let data = NSData(contentsOfURL: URL) {
-//        
-//        if let image = UIImage(data: data) {
-//          
-//          Cache.shared.imageCache.setObject(image, forKey: imageLocation)
-//          
-//          if self.count < posts.count - 1 {
-//            if self.count == 1 {
-//              self.delegate?.reloadTable()
-//            }
-//            self.count += 1
-//            self.downloadImage(self.posts)
-//          } else {
-//            self.count = 0
-//          }
-//        }
-//      }
-//    }
-//  }
-//  
-//  func downloadProfileImage(imageLocation: String) {
-//    
-//    if !ProfileImageTracker.imageLocations.contains(imageLocation) {
-//      
-//      let saveLocation = NSURL(fileURLWithPath: docsDirect() +  imageLocation)
-//      let storageRef = FIRStorage.storage().reference()
-//      let pathReference = storageRef.child("profileImages").child(imageLocation + ".jpg")
-//      
-//      pathReference.writeToFile(saveLocation) { (URL, error) -> Void in
-//        
-//        guard let URL = URL where error == nil else { print("Error - ", error.debugDescription); return }
-//        
-//        if let data = NSData(contentsOfURL: URL) {
-//          
-//          if let image = UIImage(data: data) {
-//            
-//            Cache.shared.profileImageCache.setObject(image, forKey: (imageLocation))
-//            ProfileImageTracker.imageLocations.insert(imageLocation)
-//            
-//          }
-//        }
-//      }
-//    }
-//  }
-//  
-//  mutating func deletePost(post: Post) {
-//    
-//    let storageImageRef = FIRStorage.storage().reference()
-//    let postRef = REF_POSTS.child(post.postKey) as FIRDatabaseReference!
-//    let userPostRef = REF_USER_CURRENT.child("posts").child(post.postKey) as FIRDatabaseReference!
-//    
-//    let deleteMethod = storageImageRef.child("images").child(post.postKey + ".jpg")
-//    
-//    deleteMethod.deleteWithCompletion({ error in
-//      
-//      guard error == nil else { print("delete error", error.debugDescription)
-//        
-//        //        DataService.ds.deletePostAtIndex(i)
-//        //        userPostRef.removeValue()
-//        //        postRef.removeValue()
-//        //
-//        //        NSNotificationCenter.defaultCenter().postNotificationName("reloadTables", object: self)
-//        
-//        return }
-//      
-//      print("storage image removed")
-//      
-//      for i in self.posts.indices {
-//        
-//        if self.posts[i].postKey == post.postKey {
-//          
-//          self.deletePostAtIndex(i)
-//          userPostRef.removeValue()
-//          postRef.removeValue()
-//          
-//          //use delegate.reloadTable instead?
-//          NSNotificationCenter.defaultCenter().postNotificationName("reloadTables", object: nil)
-//          
-//          return
-//        }
-//      }
-//      
-//      for i in self.answeredPosts.indices {
-//        
-//        if self.answeredPosts[i].postKey == post.postKey {
-//          
-//          self.deleteAnsweredPostAtIndex(i)
-//          userPostRef.removeValue()
-//          postRef.removeValue()
-//          
-//          NSNotificationCenter.defaultCenter().postNotificationName("reloadTables", object: nil)
-//          
-//          return
-//        }
-//      }
-//    })
-//    
-//  }
-//  
-//  mutating func blockUser(post: Post) {
-//    
-//    //Add blocked user to database
-//    let userRef = REF_USER_CURRENT.child("blockedUsers").child(post.userKey)
-//    userRef.setValue(post.userKey)
-//    
-//    //Remove blocked user locally and update table
-//    for i in posts {
-//      if i.postKey == post.postKey {
-//        
-//        print(i.postKey, i.username)
-//        
-//        deletePostsByBlockedUser(post.userKey)
-//        
-//      }
-//    }
-//    
-//    NSNotificationCenter.defaultCenter().postNotificationName("reloadTables", object: nil)
-//    
-//    downloadTableContent()
-//  }
-//  
-//  mutating func markPostAsAnswered(post: Post, answer: String) {
-//    
-//    let postRef = REF_POSTS.child(post.postKey).child("answered") as FIRDatabaseReference!
-//    
-//    postRef.setValue(answer)
-//    
-//    for i in posts.indices {
-//      
-//      if posts[i].postKey == post.postKey {
-//        
-//        deletePostAtIndex(i)
-//        
-//        NSNotificationCenter.defaultCenter().postNotificationName("reloadTables", object: nil)
-//        
-//        return
-//      }
-//    }
-//  }
-//  
-//  func reportPost(post: Post, reason: String) {
-//    
-//    let postRef = REF_BASE.child("reportedPosts").child(post.postKey).child(
-//      currentUserKey!) as FIRDatabaseReference!
-//    
-//    postRef.setValue(reason)
-//    
-//    //Post needs to be marked as reported or deleted
-//    
-//  }
-//  
-//}
-
 class DataService: HelperFunctions {
   
   static let ds = DataService()
@@ -482,7 +49,7 @@ class DataService: HelperFunctions {
 
   var currentUserKey: String? {
     
-    return NSUserDefaults.standardUserDefaults().valueForKey(Constants().KEY_UID) as? String ?? nil
+    return NSUserDefaults.standardUserDefaults().valueForKey(Constants.KEY_UID) as? String ?? nil
   }
   
   var usernames: [String: String] {
@@ -636,7 +203,7 @@ class DataService: HelperFunctions {
           if let userDict = snap.value as? [String: AnyObject] {
             
             if let name = userDict["username"] as? String {
-              print(name, snap.key)
+//              print(name, snap.key)
               self._usernames[snap.key] = name
             }
           }
@@ -657,7 +224,7 @@ class DataService: HelperFunctions {
         for snap in snapshots {
           
           if let postDict = snap.value as? [String: AnyObject] {
-            print(postDict)
+//            print(postDict)
             let key = snap.key
             let post = Post(postKey: key, dictionary: postDict)
             
@@ -699,9 +266,21 @@ class DataService: HelperFunctions {
     })
   }
   
+  func resizeImage(image: UIImage, newWidth: CGFloat) -> UIImage {
+    
+    let scale = newWidth / image.size.width
+    let newHeight = image.size.height * scale
+    UIGraphicsBeginImageContext(CGSizeMake(newWidth, newHeight))
+    image.drawInRect(CGRectMake(0, 0, newWidth, newHeight))
+    let newImage = UIGraphicsGetImageFromCurrentImageContext()
+    UIGraphicsEndImageContext()
+    
+    return newImage!
+  }
+  
   func downloadImage(posts: [Post]) {
-    print("POSTS COUNT", count)
-    print(posts[count])
+//    print("POSTS COUNT", count)
+//    print(posts[count])
     
     guard let imageLocation = posts[count].imageUrl else { print("no image");
       
@@ -718,7 +297,7 @@ class DataService: HelperFunctions {
       return }
     
     guard Cache.shared.imageCache.objectForKey(imageLocation) as? UIImage == nil else {
-      print("image already downloaded")
+//      print("image already downloaded")
       
       if self.count < posts.count - 1 {
         
@@ -744,13 +323,19 @@ class DataService: HelperFunctions {
         
         if let image = UIImage(data: data) {
           
-          Cache.shared.imageCache.setObject(image, forKey: imageLocation)
+          let newImage = self.resizeImage(image, newWidth: 414)
+          
+          Cache.shared.imageCache.setObject(newImage, forKey: imageLocation)
+          
+//          Cache.shared.imageCache.setObject(image, forKey: imageLocation)
           
           if self.count < posts.count - 1 {
             if self.count == 1 {
+              
               self.delegate?.reloadTable()
             }
             self.count += 1
+            
             self.downloadImage(self.posts)
           } else {
             self.count = 0
@@ -848,7 +433,7 @@ class DataService: HelperFunctions {
     for i in DataService.ds.posts {
       if i.postKey == post.postKey {
         
-        print(i.postKey, i.username)
+//        print(i.postKey, i.username)
         
         DataService.ds.deletePostsByBlockedUser(post.userKey)
         
